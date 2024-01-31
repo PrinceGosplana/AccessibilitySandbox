8595dbede8b125dd00eb798e0ba5631af8f82917
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-31 19:44:27 +0200

    init the project

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by OLEKSANDR ISAIEV on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
