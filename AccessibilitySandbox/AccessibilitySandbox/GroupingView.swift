d106dcae29606ba3cd5692e096e0776f96cf236b
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-08 06:26:23 +0200

    add new view and image

---
+++ AccessibilitySandbox/AccessibilitySandbox/GroupingView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupingView.swift
+//  AccessibilitySandbox
+//
+//  Created by OLEKSANDR ISAIEV on 08.02.2024.
+//
+
+import SwiftUI
+
+struct GroupingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupingView()
+}
