c263c49179028dc5b2c9e48a087c4230d7d04511
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-10 17:49:15 +0200

    read all labels from vstack

--- AccessibilitySandbox/AccessibilitySandbox/GroupingView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/GroupingView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct GroupingView: View {
     var body: some View {
-        Image(decorative: "redDemon")
-            .resizable()
-            .scaledToFit()
-            .accessibilityHidden(true)
+//        Image(decorative: "redDemon")
+//            .resizable()
+//            .scaledToFit()
+//            .accessibilityHidden(true)
+        VStack {
+            Text("Your score is")
+            Text("1000")
+                .font(.title)
+        }
+        .accessibilityElement(children: .combine)
     }
 }
 
