01354ea026c91c1b26c013be77e194ba9ef9d3e4
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-10 17:54:44 +0200

    new view

---
+++ AccessibilitySandbox/AccessibilitySandbox/SeparateReadingView.swift
@@ -0,0 +1,18 @@
+//
+//  SeparateReadingView.swift
+//  AccessibilitySandbox
+//
+//  Created by OLEKSANDR ISAIEV on 10.02.2024.
+//
+
+import SwiftUI
+
+struct SeparateReadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeparateReadingView()
+}
