3f06a5167342b56764591299c58b2d380d23d573
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-11 07:16:54 +0200

    add different options for spelling one difficult name

--- AccessibilitySandbox/AccessibilitySandbox/VoiceInputView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/VoiceInputView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct VoiceInputView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("John Fitzgerald Kennedy") {
+            print("button tapped")
+        }
+        .accessibilityInputLabels(["John Fitzgerald Kennedy", "Kennedy", "JFK"])
     }
 }
 
