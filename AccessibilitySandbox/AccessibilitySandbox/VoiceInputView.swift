dba6f752e5f5bdcd5a651f006f1ad65e1ed8ce03
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-11 07:01:20 +0200

    add empty view for voice input example

---
+++ AccessibilitySandbox/AccessibilitySandbox/VoiceInputView.swift
@@ -0,0 +1,18 @@
+//
+//  VoiceInputView.swift
+//  AccessibilitySandbox
+//
+//  Created by OLEKSANDR ISAIEV on 11.02.2024.
+//
+
+import SwiftUI
+
+struct VoiceInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VoiceInputView()
+}
